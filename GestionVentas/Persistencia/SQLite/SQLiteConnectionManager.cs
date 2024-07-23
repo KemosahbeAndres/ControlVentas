b281db2a52a0dcbd06834cc57615bf33b6b2ff2b
Andres Cubillos <andrestj1996@hotmail.com>
2024-07-23 16:12:40 -0400

    ajustando base de datos SQLite

---
+++ GestionVentas/Persistencia/SQLite/SQLiteConnectionManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionInventario.Persistence.SQLite
+{
+    class SQLiteConnectionManager
+    {
+        private static SQLiteConnection instance;
+
+        public static SQLiteConnection getConnection()
+        {
+            if(instance == null)
+            {
+                instance = new SQLiteConnection();
+            }
+            return instance;
+        }
+    }
+}
